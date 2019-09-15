6b470a2d500d1fd50fd982432c9ed9be7d0a37d3
Aram Drevekenin <aram@poor.dev>
2019-09-15 17:18:21 +0200

    refactor(store): macros to functions

--- src/store/current_connections.rs
+++ src/store/current_connections.rs
@@ -1,32 +1,34 @@
 use crate::traffic::{Connection, Protocol};
 
 use ::std::collections::HashMap;
-use ::std::net::IpAddr;
+use ::std::net::{IpAddr, Ipv4Addr};
 use ::netstat::{SocketInfo, ProtocolSocketInfo};
 
-macro_rules! get_ipv4_address {
-    ($a:expr) => {
-        match $a {
-            IpAddr::V4(addr) => Some(addr),
-            IpAddr::V6(_) => None
-        }
+fn get_ipv4_address(ip: IpAddr) -> Option<Ipv4Addr> {
+    match ip {
+        IpAddr::V4(addr) => Some(addr),
+        IpAddr::V6(_) => None
     }
 }
 
-macro_rules! build_ipv4_connection {
-    ($a:expr, $b:expr, $c:expr, $d:expr) => {
-        match ($a, $b) {
-            (Some(local_ip), Some(remote_ip)) => {
-                Some(Connection {
-                    local_ip,
-                    remote_ip,
-                    local_port: $c.local_port,
-                    remote_port: $c.remote_port,
-                    protocol: $d
-                })
-            },
-            (_, _) => None
-        }
+fn build_ipv4_connection (
+    local_ip: Option<Ipv4Addr>,
+    remote_ip: Option<Ipv4Addr>,
+    local_port: u16,
+    remote_port: u16,
+    protocol: Protocol
+) -> Option<Connection> {
+    match (local_ip, remote_ip) {
+        (Some(local_ip), Some(remote_ip)) => {
+            Some(Connection {
+                local_ip,
+                remote_ip,
+                local_port,
+                remote_port,
+                protocol
+            })
+        },
+        (_, _) => None
     }
 }
 
@@ -41,25 +43,17 @@ impl CurrentConnections {
         for si in sockets_info {
             match si.protocol_socket_info {
                 ProtocolSocketInfo::Tcp(tcp_si) => {
-                    let local_addr = get_ipv4_address!(tcp_si.local_addr);
-                    let remote_addr = get_ipv4_address!(tcp_si.remote_addr);
-                    let connection = build_ipv4_connection!(local_addr, remote_addr, tcp_si, Protocol::Tcp);
-                    match connection {
-                        Some(conn) => {
-                            connections.insert(conn, si.associated_pids.iter().map(|pid| get_process_name(*pid as i32).unwrap()).collect()); // TODO: handle None
-                        },
-                        None => ()
+                    let local_addr = get_ipv4_address(tcp_si.local_addr);
+                    let remote_addr = get_ipv4_address(tcp_si.remote_addr);
+                    if let Some(conn) = build_ipv4_connection(local_addr, remote_addr, tcp_si.local_port, tcp_si.remote_port, Protocol::Tcp) {
+                        connections.insert(conn, si.associated_pids.iter().map(|pid| get_process_name(*pid as i32).unwrap()).collect()); // TODO: handle None
                     }
                 },
                 ProtocolSocketInfo::Udp(udp_si) => {
-                    let local_addr = get_ipv4_address!(udp_si.local_addr);
-                    let remote_addr = get_ipv4_address!(udp_si.remote_addr);
-                    let connection = build_ipv4_connection!(local_addr, remote_addr, udp_si, Protocol::Udp);
-                    match connection {
-                        Some(conn) => {
-                            connections.insert(conn, si.associated_pids.iter().map(|pid| get_process_name(*pid as i32).unwrap()).collect());
-                        },
-                        None => ()
+                    let local_addr = get_ipv4_address(udp_si.local_addr);
+                    let remote_addr = get_ipv4_address(udp_si.remote_addr);
+                    if let Some(conn) = build_ipv4_connection(local_addr, remote_addr, udp_si.local_port, udp_si.remote_port, Protocol::Udp) {
+                        connections.insert(conn, si.associated_pids.iter().map(|pid| get_process_name(*pid as i32).unwrap()).collect());
                     }
                 }
             }
