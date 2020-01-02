38f8bf95e72f04ef95cc632b01efe642b1853c8c
Simone Robutti <simone.robutti@protonmail.com>
2020-01-02 11:28:52 +0100

    renamed sleep events function

--- src/tests/cases/test_utils.rs
+++ src/tests/cases/test_utils.rs
@@ -3,7 +3,7 @@ use std::iter;
 
 use ::termion::event::{Event, Key};
 
-pub fn sleep_ctrl_c_keyboard_events(sleep_num: usize) -> Box<KeyboardEvents> {
+pub fn sleep_and_quit_events(sleep_num: usize) -> Box<KeyboardEvents> {
     let mut events: Vec<Option<Event>> = iter::repeat(None).take(sleep_num).collect();
     events.push(Some(Event::Key(Key::Ctrl('c'))));
     Box::new(KeyboardEvents::new(events))
