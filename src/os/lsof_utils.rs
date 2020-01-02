1eb4867b23ed7d720d31b62589d1ccef08fd2ab4
Kelvin Zhang <zhangxp1998@gmail.com>
2020-01-02 12:18:45 -0500

    Display 30 chars for process name

--- src/os/lsof_utils.rs
+++ src/os/lsof_utils.rs
@@ -62,7 +62,7 @@ impl RawConnection {
 }
 
 pub fn get_connections<'a>() -> RawConnections {
-    let content = run(&["-n", "-P", "-i4"]);
+    let content = run(&["-n", "-P", "-i4", "+c", "30"]);
     RawConnections::new(content)
 }
 
