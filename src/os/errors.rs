8f65f00abd05fd9c0c9c6536c6d194d24ccacf1e
John <sergiybiluk@gmail.com>
2020-02-07 21:13:18 +0100

    remove unused code

--- src/os/errors.rs
+++ src/os/errors.rs
@@ -6,14 +6,6 @@ use failure::{Backtrace, Context, Fail};
 pub struct GetInterfaceError {
     inner: Context<GetInterfaceErrorKind>,
 }
-impl GetInterfaceError {
-    pub fn new(kind: GetInterfaceErrorKind) -> GetInterfaceError {
-        GetInterfaceError::from(kind)
-    }
-    pub fn kind(&self) -> GetInterfaceErrorKind {
-        self.inner.get_context().clone()
-    }
-}
 
 impl From<GetInterfaceErrorKind> for GetInterfaceError {
     fn from(kind: GetInterfaceErrorKind) -> GetInterfaceError {
