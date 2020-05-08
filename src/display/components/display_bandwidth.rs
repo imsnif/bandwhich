ae3ca6e5f1bb4f9b03f30f8291cc10b9aec07bb8
Brooks Rady <b.j.rady@gmail.com>
2020-05-08 15:18:53 +0100

    fix(formatting): add terabytes as a display unit (#168)

--- src/display/components/display_bandwidth.rs
+++ src/display/components/display_bandwidth.rs
@@ -8,7 +8,9 @@ pub struct DisplayBandwidth {
 impl fmt::Display for DisplayBandwidth {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let suffix = if self.as_rate { "ps" } else { "" };
-        if self.bandwidth > 999_999_999.0 {
+        if self.bandwidth > 999_999_999_999.0 {
+            write!(f, "{:.2}TB{}", self.bandwidth / 1_000_000_000_000.0, suffix)
+        } else if self.bandwidth > 999_999_999.0 {
             write!(f, "{:.2}GB{}", self.bandwidth / 1_000_000_000.0, suffix)
         } else if self.bandwidth > 999_999.0 {
             write!(f, "{:.2}MB{}", self.bandwidth / 1_000_000.0, suffix)
