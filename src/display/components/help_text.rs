f2954a009ac3cdba1b7f57a0fd846f18d290d3a5
llesage <louis.lesage@umontreal.ca>
2020-05-17 13:59:34 -0400

    Fixed help layout and added tests

--- src/display/components/help_text.rs
+++ src/display/components/help_text.rs
@@ -9,7 +9,8 @@ pub struct HelpText {
     pub show_dns: bool,
 }
 
-const FIRST_WIDTH_BREAKPOINT: u16 = 77;
+const FIRST_WIDTH_BREAKPOINT: u16 = 76;
+const SECOND_WIDTH_BREAKPOINT: u16 = 54;
 
 const TEXT_WHEN_PAUSED: &str = " Press <SPACE> to resume.";
 const TEXT_WHEN_NOT_PAUSED: &str = " Press <SPACE> to pause.";
@@ -26,20 +27,22 @@ impl HelpText {
                 TEXT_WHEN_NOT_PAUSED
             };
 
-            let dns_content = if self.show_dns {
+            let dns_content = if rect.width <= FIRST_WIDTH_BREAKPOINT {
+                ""
+            } else if self.show_dns {
                 TEXT_WHEN_DNS_SHOWN
             } else {
                 TEXT_WHEN_DNS_NOT_SHOWN
             };
 
-            let tab_text = if rect.width >= FIRST_WIDTH_BREAKPOINT {
-                TEXT_TAB_TIP
-            } else {
+            let tab_text = if rect.width <= SECOND_WIDTH_BREAKPOINT {
                 ""
+            } else {
+                TEXT_TAB_TIP
             };
 
             [Text::styled(
-                format!("{}{}{}", pause_content, dns_content, tab_text),
+                format!("{}{}{}", pause_content, tab_text, dns_content),
                 Style::default().modifier(Modifier::BOLD),
             )]
         };
