6ccd2bf701eaac5ea5be87df73483a2bdeca3941
Aram Drevekenin <aram@poor.dev>
2020-09-01 09:14:46 +0200

    style(format): line length

--- src/display/components/display_bandwidth.rs
+++ src/display/components/display_bandwidth.rs
@@ -9,7 +9,8 @@ impl fmt::Display for DisplayBandwidth {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let suffix = if self.as_rate { "ps" } else { "" };
         if self.bandwidth > 999_999_999_999.0 {
-            write!(f, "{:.2}TB{}", self.bandwidth / 1_099_511_627_776.0, suffix) // 1024 * 1024 * 1024 * 1024
+            // 1024 * 1024 * 1024 * 1024
+            write!(f, "{:.2}TB{}", self.bandwidth / 1_099_511_627_776.0, suffix)
         } else if self.bandwidth > 999_999_999.0 {
             write!(f, "{:.2}GB{}", self.bandwidth / 1_073_741_824.0, suffix) // 1024 * 1024 * 1024
         } else if self.bandwidth > 999_999.0 {
