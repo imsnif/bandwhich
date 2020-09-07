e40e88a49510b6e869bf2b56fe8fd8c796755696
Remigiusz Godowicz <rg1993@o2.pl>
2020-09-07 22:24:04 +0200

    fix(build): run build.rs only once

--- build.rs
+++ build.rs
@@ -10,17 +10,16 @@ fn download_winpcap_sdk() {
     use std::fs::File;
     use std::io::prelude::*;
 
+    println!("cargo:rerun-if-changed=build.rs");
+
     let out_dir = env::var("OUT_DIR").unwrap();
 
     let mut reader = Vec::new();
-    let res = request::get(
+    let _res = request::get(
         "https://nmap.org/npcap/dist/npcap-sdk-1.05.zip",
         &mut reader,
     )
     .unwrap();
-
-    println!("Status: {} {}", res.status_code(), res.reason());
-    println!("Reader len: {}", reader.len());
 
     let mut pcapzip = File::create(format!("{}{}", out_dir, "/npcap.zip")).unwrap();
     pcapzip.write_all(reader.as_slice()).unwrap();
@@ -28,13 +27,15 @@ fn download_winpcap_sdk() {
 
     pcapzip = File::open(format!("{}{}", out_dir, "/npcap.zip")).unwrap();
 
+    let lib_name = "Packet.lib";
     #[cfg(target_arch = "x86_64")]
-    let lib_path = "Lib/x64/Packet.lib";
+    let lib_dir = "Lib/x64";
     #[cfg(target_arch = "x86")]
-    let lib_path = "Lib/Packet.lib";
+    let lib_dir = "Lib";
 
+    let lib_path = format!("{}/{}", lib_dir, lib_name);
     let mut zip_archive = zip::ZipArchive::new(pcapzip).unwrap();
-    let mut pcaplib = match zip_archive.by_name(lib_path) {
+    let mut pcaplib = match zip_archive.by_name(lib_path.as_str()) {
         Ok(pcaplib) => pcaplib,
         Err(err) => {
             panic!(err);
@@ -44,9 +45,10 @@ fn download_winpcap_sdk() {
     let mut pcaplib_bytes = Vec::new();
     pcaplib.read_to_end(&mut pcaplib_bytes).unwrap();
 
-    let mut pcaplib_file = File::create(format!("{}{}", out_dir, "/Packet.lib")).unwrap();
+    std::fs::create_dir_all(format!("{}/{}", out_dir, lib_dir)).unwrap();
+    let mut pcaplib_file = File::create(format!("{}/{}", out_dir, lib_path)).unwrap();
     pcaplib_file.write_all(pcaplib_bytes.as_slice()).unwrap();
     pcaplib_file.flush().unwrap();
 
-    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-search=native={}/{}", out_dir, lib_dir);
 }
